c1fa9e40939da831d0b6c955e603ec4c27cd33a9
EricFukuyama <ericfukuyama@hotmail.com>
2023-11-18 15:59:22 -0300

    tentativa  3 lcd

--- TicTacToe/lcd.py
+++ TicTacToe/lcd.py
@@ -1,22 +1,44 @@
-import i2c_lcd_driver  
-from time import sleep  
-lcd=i2c_lcd_driver.lcd()  
-lcd.clear()  
-lcd.display("Testing....",1,0)  
-sleep(1)
-lcd.clear()
-for j in range(1,3):
-    for i in range(16):
-        lcd.display("*",j,i)
-        sleep(0.1)
-lcd.clear()   
-while True:  
-    try: 
-        lcd.display("Enter String you",1,0)
-        lcd.display("want to display",2,0) 
-        sleep(1)
-        lcd.display(input("Enter String You Want to Display"),1,0)
-        sleep(2)
-        lcd.clear()    
-    except KeyboardInterrupt:    
-        break;  
+#!/usr/bin/python3
+
+"""
+    Program: LCD1602 Demo (lcd-hello.py)
+    Author:  M. Heidenreich, (c) 2020
+
+    Description:
+    
+    This code is provided in support of the following YouTube tutorial:
+    https://youtu.be/DHbLBTRpTWM
+
+    This example shows how to use the LCD1602 I2C display with Raspberry Pi.
+
+    THIS SOFTWARE AND LINKED VIDEO TOTORIAL ARE PROVIDED "AS IS" AND THE
+    AUTHOR DISCLAIMS ALL WARRANTIES INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
+
+lcd = LCD()
+
+def safe_exit(signum, frame):
+    exit(1)
+
+try:
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
+
+    lcd.text("Hello,", 1)
+    lcd.text("Raspberry Pi!", 2)
+
+    pause()
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    lcd.clear()
